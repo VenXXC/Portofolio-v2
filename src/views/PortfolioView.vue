8106f477191b4e37b660da33a46f26486373e720
painzyzcdrx <muhammadrafif0110@gmail.com>
2025-04-01 19:12:34 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -7,45 +7,45 @@ export default {
         {
           id: 1,
           name: 'Personal Website',
-          imageUrl: 'portfolio_v2',
+          imageUrl: 'https://files.catbox.moe/x3gnuh.jpg',
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
-          tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/Portofolio-v2',
-          demo: 'https://bagasrakha.netlify.app/'
+          tech: 'Next Js, Tailwind',
+          github: 'https://github.com/painzy740/Profile/',
+          demo: 'https://painzy.web.id/'
         },
         {
           id: 2,
-          name: 'Sahabat Muslim',
-          imageUrl: 'sahabatmuslim',
-          status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
-          tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/muslim-companion',
-          demo: 'https://sahabat-muslim.netlify.app/'
+          name: 'Website Chat Ai + Art ai',
+          imageUrl: 'https://files.catbox.moe/uqwjm1.jpg',
+          status: 'Website Artificial Intelligence Chat And Art',
+          tech: 'React Js, Tailwind',
+          github: 'https://github.com/painzy740/strai',
+          demo: 'https://staries-ai.painzysite.biz.id/'
         },
         {
           id: 3,
-          name: 'SISTEKDES',
-          imageUrl: 'sistekdes',
-          status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
-          tech: 'Laravel 9, Bootstrap',
+          name: 'Coming Soon',
+          imageUrl: 'https://files.catbox.moe/cobelq.png',
+          status: 'Coming Soon.',
+          tech: 'Unknown',
           github: 'null',
           demo: 'null'
         },
         {
           id: 4,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
+          name: 'Coming Soon',
+          imageUrl: 'https://files.catbox.moe/cobelq.png',
           status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
-          tech: 'Laravel 8, Bootstrap',
+          tech: 'Unknown',
           github: 'null',
           demo: 'null'
         },
         {
           id: 5,
-          name: 'SP2SE',
-          imageUrl: 'sp2se',
-          status: 'Sistem Percepatan Pelayanan Surat Elektronik, A web application to manage letters in the village.',
-          tech: 'Codeigniter 3, Bootstrap',
+          name: 'Coming Soon',
+          imageUrl: 'https://files.catbox.moe/cobelq.png',
+          status: 'Coming Soon.',
+          tech: 'Unknown',
           github: 'null',
           demo: 'null'
         }
